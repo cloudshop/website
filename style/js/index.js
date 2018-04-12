87a1fe3dd5fbfefe08b454e868343e80b38e1899
qijianbei <17600045817@163.com>
2018-04-12 09:43:46 +0800

    qijianbei

---
+++ style/js/index.js
@@ -0,0 +1,8 @@
+$(".header li").click(function() {
+    $(this).addClass('col').siblings().removeClass('col')
+    alert('敬请期待……')
+})
+
+$(".new_list li").click(function() {
+    location.href = "http://mp.weixin.qq.com/s/cRiIgEGG3GWfuOM7AVOR1g"
+})
